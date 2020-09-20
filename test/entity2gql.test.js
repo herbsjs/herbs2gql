f4b1b0c37bdd9419312cf0410da0b9fe1ec99d19
Matheus Guilherme de Araujo Vicente <matheusprgamer@hotmail.com>
2020-09-20 14:41:32 -0300

    renamed 2gql functions and implemented options2gql

--- test/entity2gql.test.js
+++ test/entity2gql.test.js
@@ -1,47 +1,89 @@
-const assert = require('assert')
-const { entity, field } = require('gotu')
-const entity2gql = require('../src/entity2gql')
+const assert = require("assert")
+const { entity, field } = require("gotu")
+const entity2gql = require("../src/entity2gql")
 
-describe('Entity 2 GQL String', () => {
+describe("Entity 2 GQL String", () => {
+  it("should convert a entity to type", async () => {
+    // given
+    const givenAnEntity = entity("Entity", {
+      stringField: field(String),
+      stringArrayField: field([String]),
+      numberField: field(Number),
+      numberArrayField: field([Number]),
+      stringWithPresence: field(String, {
+        validation: { presence: true },
+      }),
+      stringArrayWithPresence: field([String], {
+        validation: { presence: true },
+      }),
+      booleanField: field(Boolean),
+      booleanArrayField: field([Boolean]),
+      dateField: field(Date),
+      dateArrayField: field([Date]),
+      // objectField: field(Object),
+      // arrayField: field(Array),
+    })
 
-    it('should convert a entity to GQL', async () => {
-        // given
-        const givenAnEntity = entity('Entity', {
-            stringField: field(String),
-            stringArrayField: field([String]),
-            numberField: field(Number),
-            numberArrayField: field([Number]),
-            stringWithPresence: field(String, {
-                validation: { presence: true }
-            }),
-            stringArrayWithPresence: field([String], {
-                validation: { presence: true }
-            }),
-            booleanField: field(Boolean),
-            booleanArrayField: field([Boolean]),
-            dateField: field(Date),
-            dateArrayField: field([Date]),
-            // objectField: field(Object),
-            // arrayField: field(Array),
-        })
+    // when
+    const gql = entity2gql.toType(givenAnEntity)
 
-        // when
-        const gql = entity2gql(givenAnEntity)
-
-        // then
-        assert.deepStrictEqual(gql,
-            `type Entity {
+    // then
+    assert.deepStrictEqual(
+      gql,
+      `type Entity {
     stringField: String
     stringArrayField: [String]
     numberField: Float
     numberArrayField: [Float]
-    stringWithPresence: String
-    stringArrayWithPresence: [String]
+    stringWithPresence: String!
+    stringArrayWithPresence: [String]!
     booleanField: Boolean
     booleanArrayField: [Boolean]
     dateField: Date
     dateArrayField: [Date]
 }`
-        )
+    )
+  })
+
+  it("should convert a entity to input", async () => {
+    // given
+    const givenAnEntity = entity("Entity", {
+      stringField: field(String),
+      stringArrayField: field([String]),
+      numberField: field(Number),
+      numberArrayField: field([Number]),
+      stringWithPresence: field(String, {
+        validation: { presence: true },
+      }),
+      stringArrayWithPresence: field([String], {
+        validation: { presence: true },
+      }),
+      booleanField: field(Boolean),
+      booleanArrayField: field([Boolean]),
+      dateField: field(Date),
+      dateArrayField: field([Date]),
+      // objectField: field(Object),
+      // arrayField: field(Array),
     })
+
+    // when
+    const gql = entity2gql.toInput(givenAnEntity)
+
+    // then
+    assert.deepStrictEqual(
+      gql,
+      `input Entity {
+    stringField: String
+    stringArrayField: [String]
+    numberField: Float
+    numberArrayField: [Float]
+    stringWithPresence: String!
+    stringArrayWithPresence: [String]!
+    booleanField: Boolean
+    booleanArrayField: [Boolean]
+    dateField: Date
+    dateArrayField: [Date]
+}`
+    )
+  })
 })
