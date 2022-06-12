82af7ab79bde6218e30d2f16e058607fba0ead70
Pedro Marques Freire <pedromarquesfr10@gmail.com>
2022-06-11 21:55:53 -0300

    feat: created initial structure

--- src/generate.js
+++ src/generate.js
@@ -1,22 +1,19 @@
-const entity2type = require("./entity2type")
-const usecase2mutation = require("./usecase2mutation")
-const usecase2query = require("./usecase2query")
-const defaultResolver = require("./defaultResolver")
+const { entity2type, usecase2mutation, usecase2query, defaultResolver } = require("./herbs2gql")
 
 function generate(herbarium) {
-    const { usecases, entities, crud } = herbarium
+  const { usecases, entities, crud } = herbarium
 
-    const entitiesName = Array.from(entities.all.values()).map(e => e.entity)
-    const queryUseCases = usecases
-        .findBy({ operation: [crud.read, crud.readAll] })
-        .map(e => e.usecase)
-    const mutatitonUseCases = usecases
-        .findBy({ operation: [crud.create, crud.update, crud.delete] })
-        .map(e => e.usecase)
+  const entitiesName = Array.from(entities.all.values()).map(e => e.entity)
+  const queryUseCases = usecases
+    .findBy({ operation: [crud.read, crud.readAll] })
+    .map(e => e.usecase)
+  const mutatitonUseCases = usecases
+    .findBy({ operation: [crud.create, crud.update, crud.delete] })
+    .map(e => e.usecase)
 
-    const mutations = mutatitonUseCases.map(usecase => usecase2mutation(usecase(), defaultResolver(usecase)))
-    const queries = queryUseCases.map(usecase => usecase2query(usecase(), defaultResolver(usecase)))
-    const defaultSchema = [`
+  const mutations = mutatitonUseCases.map(usecase => usecase2mutation(usecase(), defaultResolver(usecase)))
+  const queries = queryUseCases.map(usecase => usecase2query(usecase(), defaultResolver(usecase)))
+  const defaultSchema = [`
     type Query {
         _: Boolean
       }
@@ -24,9 +21,9 @@ function generate(herbarium) {
       type Mutation {
         _: Boolean
       }`]
-    const types = [defaultSchema, ...entitiesName.map(entity => [entity2type(entity)])]
+  const types = [defaultSchema, ...entitiesName.map(entity => [entity2type(entity)])]
 
-    return { types, queries, mutations }
+  return { types, queries, mutations }
 }
 
 module.exports = generate
